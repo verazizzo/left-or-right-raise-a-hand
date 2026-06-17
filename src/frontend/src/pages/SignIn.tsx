d454f3b1f076d065a8efe6054b19a260828bfa9f
verazizzo <v.zizzo2@studenti.poliba.it>
2026-06-17 14:36:52 +0200

    fix errors login

--- src/frontend/src/pages/SignIn.tsx
+++ src/frontend/src/pages/SignIn.tsx
@@ -111,21 +111,13 @@ export default function SignIn(props: { disableCustomTheme?: boolean }) {
 
     if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
       setEmailError(true);
-      setEmailErrorMessage('Inserisci un indirizzo email valido.');
+      setEmailErrorMessage(t.errEmailValida);
       isValid = false;
     } else {
       setEmailError(false);
       setEmailErrorMessage('');
     }
 
-    if (!password.value || password.value.length < 6) {
-      setPasswordError(true);
-      setPasswordErrorMessage('La password deve contenere almeno 6 caratteri.');
-      isValid = false;
-    } else {
-      setPasswordError(false);
-      setPasswordErrorMessage('');
-    }
 
     return isValid;
   };
